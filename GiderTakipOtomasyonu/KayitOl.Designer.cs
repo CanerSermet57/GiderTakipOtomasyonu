e9e0019732fc50f1dc99655acc37fcd44c285763
Okawai-koto <180290063@firat.edu.tr>
2022-03-05 19:04:55 +0300

    kayit ol formu olusturuldu button ve textboxlar olusturuldu adlari degistirildi

--- GiderTakipOtomasyonu/KayitOl.Designer.cs
+++ GiderTakipOtomasyonu/KayitOl.Designer.cs
@@ -29,30 +29,82 @@ namespace GiderTakipOtomasyonu
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.textBoxSirketAdi = new System.Windows.Forms.TextBox();
+            this.textBoxVergiDaireNo = new System.Windows.Forms.TextBox();
+            this.textBoxEpostaAdresi = new System.Windows.Forms.TextBox();
+            this.textBoxSifre = new System.Windows.Forms.TextBox();
+            this.textBoxSifreTekrar = new System.Windows.Forms.TextBox();
+            this.buttonKayitOl = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // textBoxKullaniciAdi
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.textBoxKullaniciAdi.Location = new System.Drawing.Point(192, 105);
+            this.textBoxKullaniciAdi.Name = "textBoxKullaniciAdi";
+            this.textBoxKullaniciAdi.Size = new System.Drawing.Size(187, 20);
+            this.textBoxKullaniciAdi.TabIndex = 0;
+            this.textBoxKullaniciAdi.Text = "Kullanici Adi";
             // 
-            // textBox1
+            // textBoxSirketAdi
             // 
-            this.textBox1.Location = new System.Drawing.Point(192, 105);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(187, 20);
-            this.textBox1.TabIndex = 0;
+            this.textBoxSirketAdi.Location = new System.Drawing.Point(192, 144);
+            this.textBoxSirketAdi.Name = "textBoxSirketAdi";
+            this.textBoxSirketAdi.Size = new System.Drawing.Size(187, 20);
+            this.textBoxSirketAdi.TabIndex = 1;
+            this.textBoxSirketAdi.Text = "Sirket Adi";
+            // 
+            // textBoxVergiDaireNo
+            // 
+            this.textBoxVergiDaireNo.Location = new System.Drawing.Point(192, 183);
+            this.textBoxVergiDaireNo.Name = "textBoxVergiDaireNo";
+            this.textBoxVergiDaireNo.Size = new System.Drawing.Size(187, 20);
+            this.textBoxVergiDaireNo.TabIndex = 2;
+            this.textBoxVergiDaireNo.Text = "Vergi Daire No";
+            // 
+            // textBoxEpostaAdresi
+            // 
+            this.textBoxEpostaAdresi.Location = new System.Drawing.Point(192, 225);
+            this.textBoxEpostaAdresi.Name = "textBoxEpostaAdresi";
+            this.textBoxEpostaAdresi.Size = new System.Drawing.Size(187, 20);
+            this.textBoxEpostaAdresi.TabIndex = 3;
+            this.textBoxEpostaAdresi.Text = "E-posta Adresi";
+            // 
+            // textBoxSifre
+            // 
+            this.textBoxSifre.Location = new System.Drawing.Point(192, 267);
+            this.textBoxSifre.Name = "textBoxSifre";
+            this.textBoxSifre.Size = new System.Drawing.Size(187, 20);
+            this.textBoxSifre.TabIndex = 4;
+            this.textBoxSifre.Text = "Sifre";
+            // 
+            // textBoxSifreTekrar
+            // 
+            this.textBoxSifreTekrar.Location = new System.Drawing.Point(192, 311);
+            this.textBoxSifreTekrar.Name = "textBoxSifreTekrar";
+            this.textBoxSifreTekrar.Size = new System.Drawing.Size(187, 20);
+            this.textBoxSifreTekrar.TabIndex = 5;
+            this.textBoxSifreTekrar.Text = "Sifre Tekrar";
+            // 
+            // buttonKayitOl
+            // 
+            this.buttonKayitOl.Location = new System.Drawing.Point(243, 359);
+            this.buttonKayitOl.Name = "buttonKayitOl";
+            this.buttonKayitOl.Size = new System.Drawing.Size(75, 23);
+            this.buttonKayitOl.TabIndex = 6;
+            this.buttonKayitOl.Text = "Kayit Ol";
+            this.buttonKayitOl.UseVisualStyleBackColor = true;
             // 
             // KayitOl
             // 
             this.ClientSize = new System.Drawing.Size(584, 565);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonKayitOl);
+            this.Controls.Add(this.textBoxSifreTekrar);
+            this.Controls.Add(this.textBoxSifre);
+            this.Controls.Add(this.textBoxEpostaAdresi);
+            this.Controls.Add(this.textBoxVergiDaireNo);
+            this.Controls.Add(this.textBoxSirketAdi);
+            this.Controls.Add(this.textBoxKullaniciAdi);
             this.Name = "KayitOl";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -62,7 +114,13 @@ namespace GiderTakipOtomasyonu
 
 
         #endregion
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        
+        private System.Windows.Forms.TextBox textBoxKullaniciAdi;
+        private System.Windows.Forms.TextBox textBoxSirketAdi;
+        private System.Windows.Forms.TextBox textBoxVergiDaireNo;
+        private System.Windows.Forms.TextBox textBoxEpostaAdresi;
+        private System.Windows.Forms.TextBox textBoxSifre;
+        private System.Windows.Forms.TextBox textBoxSifreTekrar;
+        private System.Windows.Forms.Button buttonKayitOl;
     }
 }
