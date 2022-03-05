511070bb617bf46d646e519e55601528e2965262
Okawai-koto <180290063@firat.edu.tr>
2022-03-05 18:12:12 +0300

    user admin classlari ve degiskenleri olusturuldu kayit ol formu acildi ve program.cs de olusturlmasi icin bilgiler girildi

--- GiderTakipOtomasyonu/Program.cs
+++ GiderTakipOtomasyonu/Program.cs
@@ -17,6 +17,7 @@ namespace GiderTakipOtomasyonu
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Application.Run(new KayitOl());
         }
     }
 }
