05a4f4053e8a15bcc1e395b9a83457b28567f9cf
Okawai-koto <180290063@firat.edu.tr>
2022-03-05 17:29:31 +0300

    admin ve user classlari olusturuldu degiskenler null deger atandi ve button kismiyla alakali hatalar duzeltildi (button yazan her yere button3 girildi)

---
+++ GiderTakipOtomasyonu/Classes/Admin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiderTakipOtomasyonu.Classes
+{
+    class Admin
+    {
+        public string id = null;
+        public string kullaniciAdi = null;
+        public string sifre = null;
+        public string email= null;
+        public string sirketAdi = null;
+
+
+
+
+
+
+    }
+}
---
+++ GiderTakipOtomasyonu/Classes/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiderTakipOtomasyonu.Classes
+{
+    class User
+    {
+        public string id = null;
+        public string kullaniciAdi = null;
+        public string sifre = null;
+        public string email= null;
+        public string sirketAdi = null;
+
+    }
+}
