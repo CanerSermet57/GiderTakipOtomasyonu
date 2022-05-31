bc9e365c963d8f0b2b5539df0317a2b716b18114
Tummy <Tummy@DESKTOP-09JU0MJ>
2022-05-31 20:51:29 +0300

    First Code İle Boş Veritabanı Oluşturulması

---
+++ GiderTakipOtomasyonu/Data/gtoDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GiderTakipOtomasyonu.Data
+{
+    public class gtoDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=.; Database=GTODB; Integrated Security=yes");
+
+        }
+
+    }
+}
