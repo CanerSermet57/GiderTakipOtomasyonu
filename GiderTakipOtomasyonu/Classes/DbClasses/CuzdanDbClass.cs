f1c9615844ea6a699a7038c50f7c05dd846918e5
Tummy <Tummy@DESKTOP-09JU0MJ>
2022-06-03 12:23:23 +0300

    CuzdanDbClass Sınıfının Oluşturulması

---
+++ GiderTakipOtomasyonu/Classes/DbClasses/CuzdanDbClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiderTakipOtomasyonu
+{
+    public class CuzdanDbClass
+    {
+        public int id { get; set; }
+
+        public float bakiyeTL { get; set; }
+        public float bakiyeEURO { get; set; }
+        public float bakiyeDolar { get; set; }
+    }
+}
