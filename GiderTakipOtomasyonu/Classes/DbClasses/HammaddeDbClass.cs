5b5928d343a1efeca967bc783e9a4a09c66fb75a
Tummy <Tummy@DESKTOP-09JU0MJ>
2022-06-05 02:55:30 +0300

    HammaddeDbClass Sınıfının Oluşturulması

---
+++ GiderTakipOtomasyonu/Classes/DbClasses/HammaddeDbClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiderTakipOtomasyonu
+{
+    public class HammaddeDbClass
+    {
+        public int id { get; set; }
+        public string adi { get; set; }
+        public float agirlik { get; set; }
+        public float fiyat { get; set; }
+        public bool stokDurumu { get; set; }
+
+    }
+}
