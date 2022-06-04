cde551e5515fa7366e07b361f143a930a4a72352
Tummy <Tummy@DESKTOP-09JU0MJ>
2022-06-04 07:41:41 +0300

    KategoriDbClass Sınıfının Oluşturulması

---
+++ GiderTakipOtomasyonu/Classes/DbClasses/KategoriDbClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiderTakipOtomasyonu
+{
+    public class KategoriDbClass
+    {
+        public int id { get; set; }
+
+        public string adi { get; set; }
+        
+    }
+}
