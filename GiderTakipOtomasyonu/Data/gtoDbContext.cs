fe53f51189948697e36c0cebf64b93f5759c9834
Tummy <Tummy@DESKTOP-09JU0MJ>
2022-06-04 07:17:14 +0300

    CuzdanDbClass Tablo Oluşturulması

--- GiderTakipOtomasyonu/Data/gtoDbContext.cs
+++ GiderTakipOtomasyonu/Data/gtoDbContext.cs
@@ -10,13 +10,14 @@ namespace GiderTakipOtomasyonu.Data
     public class gtoDbContext : DbContext
     {
         public DbSet<KisiDbClass> Kisiler { get; set; }
+        public DbSet<CuzdanDbClass> Cuzdanlar { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Data Source=.; Database=GTODB; Integrated Security=yes");
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // KisiDbClass Tablo Oluşturma İşlemleri
+            //******************** KisiDbClass Tablo Oluşturma İşlemleri ********************
 
             modelBuilder.Entity<KisiDbClass>().Property(x => x.bilgi)
                                               .HasColumnName("Bilgi")
@@ -43,15 +44,25 @@ namespace GiderTakipOtomasyonu.Data
 
             //******************** KisiDbClass Tablo Oluşturma İşlemleri Bitişi ********************
 
-            // KisiDbClass Tablo Test Verisi Ekleme İşlemleri
+            //******************** KisiDbClass Tablo Test Verisi Ekleme İşlemleri ********************
 
             modelBuilder.Entity<KisiDbClass>().HasData(new KisiDbClass { id = 1, borc = 501, alacak = 15100, bilgi = "Dürüst Biri", adres = "Elazığ Merkez", faksNo = "06665555555", telNo = "01472536980", adi = "Durmuş Kahraman" });
             modelBuilder.Entity<KisiDbClass>().HasData(new KisiDbClass { id = 2, borc = 540, alacak = 10560, bilgi = "İyi Biri", adres = "Ankara Kızılay", faksNo = "04565555555", telNo = "03255698547", adi = "Dilek Yılmaz" });
             modelBuilder.Entity<KisiDbClass>().HasData(new KisiDbClass { id = 3, borc = 589, alacak = 10651, bilgi = "Güvenilmez Biri", adres = "Elazığ Maden", faksNo = "02585555555", telNo = "01231471558", adi = "Caner Olmuş" });
             modelBuilder.Entity<KisiDbClass>().HasData(new KisiDbClass { id = 4, borc = 50, alacak = 5416, bilgi = "Borç Verilmez Biri", adres = "İstanbul Gaziosmanpaşa", faksNo = "02125555555", telNo = "02582695847", adi = "Emre Durhan" });
             modelBuilder.Entity<KisiDbClass>().HasData(new KisiDbClass { id = 5, borc = 500, alacak = 5510, bilgi = "Herhangi Biri", adres = "İstanbul Aksaray", faksNo = "07895555555", telNo = "04568524568", adi = "Merve Kum" });
-            //********** KisiDbClass Tablo Test Verisi Ekleme İşlemleri Bitişi **************
+            //******************** KisiDbClass Tablo Test Verisi Ekleme İşlemleri Bitişi ********************
 
+            //******************** CuzdanDbClass Tablo Oluşturma İşlemleri ********************
+
+            modelBuilder.Entity<CuzdanDbClass>().Property(x => x.bakiyeDolar)
+                                              .HasColumnName("Dolar");
+            modelBuilder.Entity<CuzdanDbClass>().Property(x => x.bakiyeEURO)
+                                              .HasColumnName("EURO");
+            modelBuilder.Entity<CuzdanDbClass>().Property(x => x.bakiyeTL)
+                                              .HasColumnName("TL");
+
+            //******************** CuzdanDbClass Tablo Oluşturma İşlemleri Bitişi ********************
 
 
         }
