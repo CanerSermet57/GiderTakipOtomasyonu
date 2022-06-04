b8bd0eb015499f2143c06bd21c2352ff215e5d27
Tummy <Tummy@DESKTOP-09JU0MJ>
2022-06-04 09:06:17 +0300

    OdemeTuruDbClass sınıfının Oluşturulması

---
+++ GiderTakipOtomasyonu/Classes/DbClasses/OdemeTuruDbClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiderTakipOtomasyonu
+{
+    public class OdemeTuruDbClass
+    {
+        public int id { get; set; }
+
+        public string adi { get; set; }
+
+        
+    }
+}
