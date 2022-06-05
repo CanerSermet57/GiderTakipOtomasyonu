26e4159aef2e90a18c8ad52284134dc85d51d6eb
Tummy <Tummy@DESKTOP-09JU0MJ>
2022-06-05 06:06:27 +0300

    TicariMallarDbClass Sınıfının Oluşturulması

---
+++ GiderTakipOtomasyonu/Classes/DbClasses/TicariMallarDbClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiderTakipOtomasyonu
+{
+    public class TicariMallarDbClass
+    {
+        public int id { get; set; }
+        public string urunAdi { get; set; }
+        public float stokSayisi { get; set; }
+        public float fiyat { get; set; }
+    }
+}
