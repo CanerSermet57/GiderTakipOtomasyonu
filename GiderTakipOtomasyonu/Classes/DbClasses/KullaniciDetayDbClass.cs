4014aa0018aa4b336d0752f177f4c38c90fac2cc
Tummy <Tummy@DESKTOP-09JU0MJ>
2022-06-10 21:56:45 +0300

    KullaniciDetayDbClass Sınıfının Oluşturulması

---
+++ GiderTakipOtomasyonu/Classes/DbClasses/KullaniciDetayDbClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiderTakipOtomasyonu
+{
+    public class KullaniciDetayDbClass
+    {
+        public int id { get; set; }
+        public string adi { get; set; }
+        public string vergiDairesi { get; set; }
+        public string vergiDairesiNo { get; set; }
+    }
+}
