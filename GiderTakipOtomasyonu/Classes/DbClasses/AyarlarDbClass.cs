7a73b6ef79ef4d01a7754b8a39934d813fd393d5
Tummy <Tummy@DESKTOP-09JU0MJ>
2022-06-10 23:00:39 +0300

    AyarlarDbClass Sınıfının Oluşturulması

---
+++ GiderTakipOtomasyonu/Classes/DbClasses/AyarlarDbClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiderTakipOtomasyonu
+{
+    public class AyarlarDbClass
+    {
+        public int id { get; set; }
+        public bool alarm { get; set; }
+        public bool bildirim { get; set; }
+
+        public TemaDbClass tema { get; set; }
+        public int? temaId { get; set; } = 1;
+    }
+}
