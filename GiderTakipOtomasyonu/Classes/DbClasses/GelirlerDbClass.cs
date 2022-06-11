55fd3ce726cfdb33c2584f4b610ae74f28dcdb7f
Tummy <Tummy@DESKTOP-09JU0MJ>
2022-06-11 15:22:40 +0300

    GelirlerDbClass Sınıfının Oluşturulması

---
+++ GiderTakipOtomasyonu/Classes/DbClasses/GelirlerDbClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiderTakipOtomasyonu
+{
+    public class GelirlerDbClass
+    {
+        public int id { get; set; }
+        public string aciklamasi { get; set; }
+        public string kime { get; set; }
+        public float tutar { get; set; }
+        public DateTime tarih { get; set; }
+
+        public OdemeTuruDbClass odemeTuru { get; set; }
+        public int? odemeTuruId { get; set; }
+
+        public KategoriDbClass Kategori { get; set; }
+        public int? kategoriId { get; set; }
+    }
+}
