9dc96fd91ef2b224eee0058b882f688b2df612d6
Okawai-koto <180290063@firat.edu.tr>
2022-06-12 01:48:46 +0300

    gelir sil arayüzünün oluşturulması veritabanından silme fonksiyonu oluşturulması sil buton yönlendirme kodunun yazılması

--- GiderTakipOtomasyonu/Forms/GelirSil.Designer.cs
+++ GiderTakipOtomasyonu/Forms/GelirSil.Designer.cs
@@ -1,6 +1,7 @@
-﻿namespace GiderTakipOtomasyonu.Forms
+﻿
+namespace GiderTakipOtomasyonu.Forms
 {
-    partial class GelirEkle
+    partial class GelirSil
     {
         /// <summary>
         /// Required designer variable.
@@ -50,7 +51,7 @@
             // numericUpDownKategori
             // 
             this.numericUpDownKategori.Font = new System.Drawing.Font("Segoe UI", 14F);
-            this.numericUpDownKategori.Location = new System.Drawing.Point(140, 272);
+            this.numericUpDownKategori.Location = new System.Drawing.Point(140, 309);
             this.numericUpDownKategori.Maximum = new decimal(new int[] {
             1000000,
             0,
@@ -58,22 +59,22 @@
             0});
             this.numericUpDownKategori.Name = "numericUpDownKategori";
             this.numericUpDownKategori.Size = new System.Drawing.Size(286, 32);
-            this.numericUpDownKategori.TabIndex = 66;
+            this.numericUpDownKategori.TabIndex = 80;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe UI", 14F);
-            this.label6.Location = new System.Drawing.Point(23, 274);
+            this.label6.Location = new System.Drawing.Point(23, 311);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(108, 25);
-            this.label6.TabIndex = 67;
+            this.label6.TabIndex = 81;
             this.label6.Text = "Kategori id:";
             // 
             // numericUpDownOdemeTuru
             // 
             this.numericUpDownOdemeTuru.Font = new System.Drawing.Font("Segoe UI", 14F);
-            this.numericUpDownOdemeTuru.Location = new System.Drawing.Point(140, 234);
+            this.numericUpDownOdemeTuru.Location = new System.Drawing.Point(140, 271);
             this.numericUpDownOdemeTuru.Maximum = new decimal(new int[] {
             1000000,
             0,
@@ -81,58 +82,58 @@
             0});
             this.numericUpDownOdemeTuru.Name = "numericUpDownOdemeTuru";
             this.numericUpDownOdemeTuru.Size = new System.Drawing.Size(286, 32);
-            this.numericUpDownOdemeTuru.TabIndex = 64;
+            this.numericUpDownOdemeTuru.TabIndex = 78;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Segoe UI", 14F);
-            this.label5.Location = new System.Drawing.Point(3, 241);
+            this.label5.Location = new System.Drawing.Point(2, 271);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(128, 25);
-            this.label5.TabIndex = 65;
+            this.label5.TabIndex = 79;
             this.label5.Text = "ÖdemetürüId:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 14F);
-            this.label3.Location = new System.Drawing.Point(74, 199);
+            this.label3.Location = new System.Drawing.Point(74, 236);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(57, 25);
-            this.label3.TabIndex = 63;
+            this.label3.TabIndex = 77;
             this.label3.Text = "Tarih:";
             // 
             // dateTimePicker1
             // 
             this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.dateTimePicker1.Location = new System.Drawing.Point(140, 199);
+            this.dateTimePicker1.Location = new System.Drawing.Point(140, 236);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(286, 29);
-            this.dateTimePicker1.TabIndex = 62;
+            this.dateTimePicker1.TabIndex = 76;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 14F);
-            this.label2.Location = new System.Drawing.Point(51, 123);
+            this.label2.Location = new System.Drawing.Point(51, 157);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(80, 25);
-            this.label2.TabIndex = 61;
+            this.label2.TabIndex = 75;
             this.label2.Text = "Kimden:";
             // 
             // textBoxKime
             // 
             this.textBoxKime.Font = new System.Drawing.Font("Segoe UI", 14F);
-            this.textBoxKime.Location = new System.Drawing.Point(140, 120);
+            this.textBoxKime.Location = new System.Drawing.Point(140, 157);
             this.textBoxKime.Name = "textBoxKime";
             this.textBoxKime.Size = new System.Drawing.Size(286, 32);
-            this.textBoxKime.TabIndex = 60;
+            this.textBoxKime.TabIndex = 74;
             // 
             // numericUpDownTutar
             // 
             this.numericUpDownTutar.Font = new System.Drawing.Font("Segoe UI", 14F);
-            this.numericUpDownTutar.Location = new System.Drawing.Point(140, 158);
+            this.numericUpDownTutar.Location = new System.Drawing.Point(140, 195);
             this.numericUpDownTutar.Maximum = new decimal(new int[] {
             1000000,
             0,
@@ -140,16 +141,16 @@
             0});
             this.numericUpDownTutar.Name = "numericUpDownTutar";
             this.numericUpDownTutar.Size = new System.Drawing.Size(286, 32);
-            this.numericUpDownTutar.TabIndex = 55;
+            this.numericUpDownTutar.TabIndex = 69;
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Segoe UI", 14F);
-            this.button1.Location = new System.Drawing.Point(140, 310);
+            this.button1.Location = new System.Drawing.Point(140, 347);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(286, 48);
-            this.button1.TabIndex = 59;
-            this.button1.Text = "Ekle";
+            this.button1.TabIndex = 73;
+            this.button1.Text = "Sil";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -157,10 +158,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 14F);
-            this.label4.Location = new System.Drawing.Point(71, 160);
+            this.label4.Location = new System.Drawing.Point(71, 197);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(60, 25);
-            this.label4.TabIndex = 56;
+            this.label4.TabIndex = 70;
             this.label4.Text = "Tutar:";
             // 
             // label8
@@ -169,34 +170,34 @@
             this.label8.Font = new System.Drawing.Font("Segoe UI", 20F);
             this.label8.Location = new System.Drawing.Point(0, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(484, 44);
-            this.label8.TabIndex = 58;
-            this.label8.Text = "Gelir Ekle";
+            this.label8.Size = new System.Drawing.Size(468, 44);
+            this.label8.TabIndex = 72;
+            this.label8.Text = "Gelir Sil";
             this.label8.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 14F);
-            this.label1.Location = new System.Drawing.Point(42, 85);
+            this.label1.Location = new System.Drawing.Point(42, 122);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(92, 25);
-            this.label1.TabIndex = 57;
+            this.label1.TabIndex = 71;
             this.label1.Text = "Açıklama:";
             // 
             // textBoxAciklama
             // 
             this.textBoxAciklama.Font = new System.Drawing.Font("Segoe UI", 14F);
-            this.textBoxAciklama.Location = new System.Drawing.Point(140, 82);
+            this.textBoxAciklama.Location = new System.Drawing.Point(140, 119);
             this.textBoxAciklama.Name = "textBoxAciklama";
             this.textBoxAciklama.Size = new System.Drawing.Size(286, 32);
-            this.textBoxAciklama.TabIndex = 54;
+            this.textBoxAciklama.TabIndex = 68;
             // 
-            // GelirEkle
+            // GelirSil
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 432);
+            this.ClientSize = new System.Drawing.Size(468, 432);
             this.Controls.Add(this.numericUpDownKategori);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.numericUpDownOdemeTuru);
@@ -211,8 +212,9 @@
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBoxAciklama);
-            this.Name = "GelirEkle";
-            this.Text = "GelirEkle";
+            this.Name = "GelirSil";
+            this.Text = "GelirSil";
+            this.Load += new System.EventHandler(this.GelirSil_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownKategori)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownOdemeTuru)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTutar)).EndInit();
