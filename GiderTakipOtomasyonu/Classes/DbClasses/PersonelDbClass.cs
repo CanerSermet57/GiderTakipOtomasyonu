ca247fda91f29e46f19b202e68c01c95385be3e0
Tummy <Tummy@DESKTOP-09JU0MJ>
2022-06-11 16:25:49 +0300

    PersonelDbClass Sınıfının Oluşturulması

---
+++ GiderTakipOtomasyonu/Classes/DbClasses/PersonelDbClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiderTakipOtomasyonu
+{
+    public class PersonelDbClass
+    {
+        public int id { get; set; }
+        public string adi { get; set; }
+        public string soyAdi { get; set; }
+        public string görevi { get; set; }
+        public float maasi { get; set; }
+        public DateTime iseBaslamaTarihi { get; set; } = new DateTime(2022, 06, 11);
+
+        public string tatilGunu { get; set; }
+    }
+}
