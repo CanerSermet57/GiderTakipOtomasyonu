7a3c619cabe35dbfcef060408d53943eaf4b06b5
Okawai-koto <180290063@firat.edu.tr>
2022-06-12 14:59:43 +0300

    giriş sayfası arayüzünün tasarlanması verilerin çekilmesi

---
+++ GiderTakipOtomasyonu/Forms/Giris.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace GiderTakipOtomasyonu.Forms
+{
+    partial class Giris
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxSifre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.label2.Location = new System.Drawing.Point(132, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 20);
+            this.label2.TabIndex = 65;
+            this.label2.Text = "Şifre:";
+            // 
+            // textBoxSifre
+            // 
+            this.textBoxSifre.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.textBoxSifre.Location = new System.Drawing.Point(195, 136);
+            this.textBoxSifre.Name = "textBoxSifre";
+            this.textBoxSifre.Size = new System.Drawing.Size(237, 32);
+            this.textBoxSifre.TabIndex = 64;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.label1.Location = new System.Drawing.Point(79, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 20);
+            this.label1.TabIndex = 63;
+            this.label1.Text = "Kullanıcı Adı:";
+            // 
+            // textBoxKullaniciAdi
+            // 
+            this.textBoxKullaniciAdi.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.textBoxKullaniciAdi.Location = new System.Drawing.Point(195, 98);
+            this.textBoxKullaniciAdi.Name = "textBoxKullaniciAdi";
+            this.textBoxKullaniciAdi.Size = new System.Drawing.Size(237, 32);
+            this.textBoxKullaniciAdi.TabIndex = 62;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.button1.Location = new System.Drawing.Point(230, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 37);
+            this.button1.TabIndex = 66;
+            this.button1.Text = "Giriş";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.button2.Location = new System.Drawing.Point(230, 250);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 37);
+            this.button2.TabIndex = 67;
+            this.button2.Text = "Kayıt Ol";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.label3.Location = new System.Drawing.Point(256, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 20);
+            this.label3.TabIndex = 68;
+            this.label3.Text = "Şifremi unuttum";
+            // 
+            // Giris
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxSifre);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxKullaniciAdi);
+            this.Name = "Giris";
+            this.Text = "Giris";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxSifre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxKullaniciAdi;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ GiderTakipOtomasyonu/Forms/Giris.cs
@@ -0,0 +1,42 @@
+﻿using GiderTakipOtomasyonu.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GiderTakipOtomasyonu.Forms
+{
+    public partial class Giris : Form
+    {
+        gtoDbContext dbContext = new gtoDbContext();
+        KullaniciDbContext kullaniciDbContext = new KullaniciDbContext();
+
+        public Giris()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            KullaniciDbClass Kullanici = new KullaniciDbClass
+            {
+                adi = textBoxKullaniciAdi.Text,
+                sifre = textBoxSifre.Text
+            };
+
+
+        }
+
+        private bool kontrol(int kullaniciadi, int sifre)
+        {
+            bool k = true;
+            return k;
+        }
+
+    }
+}
