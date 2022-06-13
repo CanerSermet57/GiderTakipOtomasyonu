7a7d0faddfe6986ba4ceff6fd4b746e728484a06
Tummy <Tummy@DESKTOP-09JU0MJ>
2022-06-13 21:21:59 +0300

    Seçilen Ticari Malın Veri Tabanında Update Edilmesi

--- GiderTakipOtomasyonu/Forms/TicariMalDüzenle.Designer.cs
+++ GiderTakipOtomasyonu/Forms/TicariMalDüzenle.Designer.cs
@@ -88,6 +88,7 @@ namespace GiderTakipOtomasyonu.Forms
             this.button1.TabIndex = 84;
             this.button1.Text = "Düzenle";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label4
             // 
