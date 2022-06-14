c5926e4e9271835960b64314254361a4c90269e2
Tummy <Tummy@DESKTOP-09JU0MJ>
2022-06-14 09:18:54 +0300

    KartlarımDbClass Sınıfının Oluşturulması

---
+++ GiderTakipOtomasyonu/Classes/DbClasses/KartlarımDbClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiderTakipOtomasyonu
+{
+    public class KartlarımDbClass
+    {
+        public int id { get; set; }
+        public string kartAdı { get; set; }
+        public string kartNo { get; set; }
+        public string sonKullanmaTarihi { get; set; }
+        public string CVCKodu { get; set; }
+        public int islemSınırı { get; set; }
+    }
+}
