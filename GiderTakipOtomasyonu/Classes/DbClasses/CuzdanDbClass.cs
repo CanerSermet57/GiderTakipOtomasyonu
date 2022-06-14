aef44dc45b94a93a5025b934cbd9c8945dd7e944
Tummy <Tummy@DESKTOP-09JU0MJ>
2022-06-14 10:01:22 +0300

    CuzdanDbClass Sınıfına Kur değişkenlerinin Eklenmesi

--- GiderTakipOtomasyonu/Classes/DbClasses/CuzdanDbClass.cs
+++ GiderTakipOtomasyonu/Classes/DbClasses/CuzdanDbClass.cs
@@ -13,5 +13,8 @@ namespace GiderTakipOtomasyonu
         public float bakiyeTL { get; set; }
         public float bakiyeEURO { get; set; }
         public float bakiyeDolar { get; set; }
+        public float kurDolar { get; set; } = 16.50F;
+        public float kurEuro { get; set; } = 17.50F;
+
     }
 }
